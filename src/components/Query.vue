f357260e379408ac824c9d53f16254a635ab69db
Evan Gillespie <evan.gillespie@leanix.net>
2019-03-07 21:48:53 -0500

    Building out Query.Vue

---
+++ src/components/Query.vue
@@ -0,0 +1,35 @@
+<script>
+
+export default{
+    name: 'Query',
+    data(){
+        return{
+            
+        }
+    }
+}
+
+</script>
+
+<!-- 
+{
+  allFactSheets(factSheetType: Application) {
+    edges {
+      node {
+        ... on Application {
+          id
+          displayName
+          relApplicationToITComponent {
+            edges {
+              node {
+                id
+                costTotalAnnual
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+-->
